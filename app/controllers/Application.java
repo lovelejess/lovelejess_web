0f75ff4ca46d242b97193b7e9d686bb6539cd3d1
lovelejess <jessicale90@gmail.com>
2014-01-24 10:33:55 -0600

    'authentication'

---
+++ app/controllers/Application.java
@@ -0,0 +1,81 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+
+import models.*;
+import views.html.*;
+public class Application extends Controller {
+
+    public static Result index() {
+       	return ok(index.render("index"));
+    }
+      // -- Authentication
+    
+    public static class Login {
+        
+        public String email;
+        public String password;
+        
+        public String validate() {
+            if(User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+        
+    }
+
+    /**
+     * Login page.
+     */
+    public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+    
+    /**
+     * Handle login form submission.
+     */
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if(loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+
+    /**
+     * Logout and clean the session.
+     */
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+            routes.Application.login()
+        );
+    }
+    public static Result eat() {
+        return ok(eat.render("eat"));
+    }
+    public static Result playy() {
+        return ok(playy.render("play"));
+    }
+    public static Result work() {
+       return ok(work.render("work"));
+    }
+    public static Result namaste() {
+        return ok(namaste.render("namaste"));
+    }
+    public static Result archivedEats(){
+        return ok(archivedEats.render("archivedEats"));
+    }
+
+}
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,44 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table eat (
+  id                        varchar(255) not null,
+  url                       varchar(255),
+  fullname                  varchar(255),
+  submitted_on              timestamp,
+  title                     varchar(255),
+  text                      varchar(255),
+  image                     varchar(255),
+  constraint pk_eat primary key (id))
+;
+
+create table account (
+  email                     varchar(255) not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  constraint pk_account primary key (email))
+;
+
+create sequence eat_seq;
+
+create sequence account_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists eat;
+
+drop table if exists account;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists eat_seq;
+
+drop sequence if exists account_seq;
+
