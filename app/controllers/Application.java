dfa65cd32d23edebe90d01e87668e09d9b866a4f
Jessica Le <jessicale90@gmail.com>
2014-01-31 22:58:20 -0600

    public view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,43 +22,27 @@ public class Application extends Controller {
             return null;
         }
     }    
-    /**
-     * Login page.
-     */
     public static Result login() {
         System.out.println("login page");
         return ok(
-            login.render(form(Login.class))
-        );
+            login.render(form(Login.class)));
     }
-
     public static Result index() {
        	return ok(index.render(""));
     }
-      // -- Authentication
-    
-    /**
-     * Handle login form submission.
-     */
     public static Result authenticate() {
         System.out.println("authenticate");
-    Form<Login> loginForm = form(Login.class).bindFromRequest();
-    if (loginForm.hasErrors()) {
-        System.out.println("has bad errors"); 
-        return badRequest(login.render(loginForm));
-    } else {
-        session("email", loginForm.get().email);
-        System.out.println("redirecting");
-        return redirect(
-            routes.Application.index()
-            //"http://www.npr.org/"
-        );
-        }
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            System.out.println("has bad errors"); 
+            return badRequest(login.render(loginForm));
+        } else {
+            session("email", loginForm.get().email);
+            System.out.println("redirecting");
+            return redirect(
+                routes.Application.index());
+            }
     }
-
-    /**
-     * Logout and clean the session.
-     */
     public static Result logout() {
         session().clear();
         flash("success", "You've been logged out");
@@ -81,5 +65,48 @@ public class Application extends Controller {
     public static Result archivedEats(){
         return ok(archivedEats.render("archivedEats"));
     }
+/*
+  * mongo.local.hostname=localhost
+  * mongo.local.port=27017
+  * mongo.remote.hostname=
+  * mongo.remote.port=25189
+  * mongo.remote.username=
+  * mongo.remote.password=
+  */
+  //   private static DB getDb() {
+  //     String userName = play.Configuration.root().getString("mongo.remote.username");
+  //     String password = play.Configuration.root().getString("mongo.remote.password");
+  //     boolean local  = true;
+    
+  //     String localHostName = play.Configuration.root().getString("mongo.local.hostname");
+  //     Integer  localPort = play.Configuration.root().getInt("mongo.local.port");
+      
+  //     String remoteHostName = play.Configuration.root().getString("mongo.remote.hostname");
+  //     Integer remotePort = play.Configuration.root().getInt("mongo.remote.port");
+
+  //     Mongo m;
+  //     DB db = null;
+  //     if(local){
+  //       String hostname = localhost;
+  //       int port = 27017;
+  //       try {
+  //       m = new Mongo( hostname, port);   
+  //       db = m.getDB( "db" );
+  //       }catch(Exception e) {
+  //          Logger.error("Exception while intiating Local MongoDB", e);    
+  //       }
+  //     }else {
+  //         String hostname = remoteHostName;
+  //           int port = remotePort;
+  //           try {
+  //           m = new Mongo( hostname , port);   
+  //           db = m.getDB( "db" );
+  //           boolean auth = db.authenticate(userName, password.toCharArray());
+  //           }catch(Exception e) {
+  //               Logger.error("Exception while intiating Local MongoDB", e);    
+  //           }
+  //     }
+  //     return db;
+  // }
 }
 
