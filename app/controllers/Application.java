7eef01b5ca1045826794ce4fa12bba8bd3fc54fe
lovelejess <jessicale90@gmail.com>
2014-03-08 18:34:17 -0600

    rm login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,46 +22,8 @@ import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
 
 public class Application extends Controller {
-
-    public static class Login {
-        
-        public String email;
-        public String password;
-        
-        public String validate() {
-            if(User.authenticate(email, password) == null) {
-                return "Invalid user or password";
-            }
-            return null;
-        }
-    }    
-    public static Result login() {
-        System.out.println("login page");
-        return ok(
-            login.render(form(Login.class)));
-    }
     public static Result index() {
        	return ok(index.render(""));
-    }
-    public static Result authenticate() {
-        System.out.println("authenticate");
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if (loginForm.hasErrors()) {
-            System.out.println("has bad errors"); 
-            return badRequest(login.render(loginForm));
-        } else {
-            session("email", loginForm.get().email);
-            System.out.println("redirecting");
-            return redirect(
-                routes.Application.index());
-            }
-    }
-    public static Result logout() {
-        session().clear();
-        flash("success", "You've been logged out");
-        return redirect(
-            routes.Application.login()
-        );
     }
     public static Result eat() {
         return ok(eat.render("eat"));
